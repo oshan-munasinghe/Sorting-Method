299644d52a1702df63729ec4352390393b6a5f0f
oshan malith <47682049+oshan-munasinghe@users.noreply.github.com>
2020-04-22 14:04:24 +0530

    selection sort add

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int n,t,i,j;
+
+  cout<<"Enter number of the element - ";
+  cin>>n;
+  int a[n];
+  for(i=0;i<n;i++){
+    cout << "Number "<<i<<" - ";
+    cin>>a[i];
+  }
+  for(i=0;i<n;i++){
+    for(j=i+1;j<n;j++){
+      if(a[i]<a[j]){
+        t=a[i];
+        a[i]=a[j];
+        a[j]=t;
+      }
+    }
+  }
+  cout<< "Sorted List is -> ";
+    for(i=0;i<n;i++)
+      cout<<"  "<<a[i];
+}
